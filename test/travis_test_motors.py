262308e081f59a588d2925036678f2d644580fbc
John Doe <you@example.com>
2018-04-14 07:28:26 +0000

    temp

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -5,7 +5,7 @@ import time
 from pimouse_ros.msg import MotorFreqs
 from pimouse_ros.srv import TimedMotion
 from geometry_msgs.msg import Twist
-from std_srvs.stv import Trigger, TriggerResponse
+from std_srvs.srv import Trigger, TriggerResponse
 
 class MotorTest(unittest.TestCase):
   def setUp(self):
